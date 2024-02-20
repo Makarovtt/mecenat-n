6541b512721c06e10b54a3a7e1cbd0b24ca29e8d
Tim <makarovt_t@mail.ru>
2024-02-20 16:12:15 +0400

    Добавление кнопки пожертовать

--- src/components/main/page-home/get-help.tsx
+++ src/components/main/page-home/get-help.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import classes from "./styles/page-home.module.css";
+import { PayUCassa } from "@/components/pay/pay-u";
 import { QuizGetHelp } from "./components/quiz/quiz-get-help";
 import { ModalCustom } from "@/components/ui/modal-custom";
 import { useState } from "react";
@@ -16,10 +16,19 @@ export function GetHelp() {
   }
 
   return (
-    <section className={classes.gethelp}>
-      <div className={classes.gethelp_block}>
-        <div className={classes.gethelp_btn} onClick={openModal}>
+    <section>
+      <div className="max-w-7xl mx-auto flex justify-center items-center">
+        <div
+          className="m-[80px] text-xl border rounded-md font-bold py-5 
+        px-16 italic text-[#126688] shadow-lg cursor-pointer hover:bg-cyan-50
+        transition"
+          onClick={openModal}
+        >
           Получить помощь
+        </div>
+        <div className="w-px border-l mx-10 h-36"></div>
+        <div className="ml-10">
+          <PayUCassa />
         </div>
       </div>
 
